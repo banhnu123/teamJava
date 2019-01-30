9ce8caed75d8d0e2ab9ee79f27830d8ece892c1e
BaKalL <ozzzkonzzzo@gmail.com>
2019-01-30 21:09:14 +0700

    First update

---
+++ src/main/java/com/example/demo/SellerWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerWebApplication.class, args);
+	}
+
+}
+
